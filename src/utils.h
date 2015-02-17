798c8f42e367ffe4a401061e99bfbb98a88a1087
Leigh Metcalf <lbmetcalf@cert.org>
2015-02-17 16:50:00 +0000

    removed extraneous functions from utils

--- src/utils.h
+++ src/utils.h
@@ -15,6 +15,3 @@ using namespace std;
 typedef map<string,int> StringIntMap;
 
 vector<string> split(string &s, char delim);
-string join(StringIntMap & vec, const string & sep);
-string join(const vector<string> & vec,const string & sep);
-int RandomInt(int low, int high);
